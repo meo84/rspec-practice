de5a456fccbf5546283ee6f68785dde40fbf667d
meo84 <oopsiday@gmail.com>
2017-03-28 13:47:53 +0200

    raises an ArgumentError when input given is not a string number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
   def self.add(input_string)
+    raise ArgumentError, 'Input is not a string number' if input_string.match(/\D/)
     input_string.to_i
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,6 +8,16 @@ describe StringCalculator do
           expect(StringCalculator.add('')).to eql(0)
         end
       end
+      context "given '4'" do
+        it "returns '4'" do
+          expect(StringCalculator.add('4')).to eql(4)
+        end
+      end
+      context "given 'S'" do
+        it "raises an Argument error for inputs that are not string numbers" do
+          expect{ StringCalculator.add('S') }.to raise_error(ArgumentError)
+        end
+      end
     end
   end
 end
